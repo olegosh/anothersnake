7af08c22c47c3517a6a8a5561435115c438342b1
Oleg Oshurkov <olegosh137@gmail.com>
2020-02-05 11:13:51 +0200

    Addition to src/draw.js: clearCanvas(), drawGrid()

--- src/draw.js
+++ src/draw.js
@@ -4,7 +4,7 @@ export function draw() {
   clearCanvas(this.options.renderElement, this.options.context);
 
   drawGrid();
-  
+
   for(let i = apples.length - 1; i >= 0; i -= 1) {
     apples[i].draw();
   }
@@ -16,4 +16,16 @@ export function draw() {
   snake.draw();
 
   log('frame');
+}
+
+function clearCanvas(canvas, context) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawGrid() {
+  for(let y = 0; y < source.grid.length; y += 1) {
+    for(let x = 0; x < source.grid[y].length; x += 1) {
+      source.grid[y][x].draw();
+    }
+  }
 }
